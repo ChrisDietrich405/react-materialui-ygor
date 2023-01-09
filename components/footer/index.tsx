7e8819dc8b49a4551ad5d0df03c85b1afd07b56e
Chris <chrisdietrich3666@gmail.com>
2023-01-09 16:37:14 -0500

    feat styling cart

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -9,10 +9,10 @@ const Footer = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by{' '}
+        {/* Powered by{' '}
         <span className={styles.logo}>
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
+        </span> */}
       </a>
     </footer>
   )
--- pages/store/[id].js
+++ pages/store/[id].js
@@ -1,45 +1,54 @@
-import React from 'react'
-
-const StoreItem = ({data}) => {
-    console.log(data)
+import { Container, Box, Grid } from "@mui/material";
+import React from "react";
+import Image from "next/image";
+const StoreItem = ({ data }) => {
   return (
-    <div>{data.title}
-    <p>{data.price}</p>
-    </div>
-  )
-}
+   
+      <Grid container spacing={2} margin="0 auto" sx={{ m: 6 }}>
+        <Grid
+          item
+          xs={3}
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          boxShadow="1px 1px 1px blue"
+        >
+          <h4>{data.title}</h4>
+          <Image src={data.image} width={111} height={111} alt={data.title} />
+          <p>{data.price}</p>
+        </Grid>
+      </Grid>
+  );
+};
 
-
-export default StoreItem
+export default StoreItem;
 
 export async function getStaticPaths() {
-    const response = await fetch("https://fakestoreapi.com/products")
-    const data = await response.json()
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
 
-    const paths = data.map((item) => {
-        return {
-            params: {
-                id: item.id.toString()
-            }
-        }
-    })
-    
+  const paths = data.map((item) => {
     return {
-        paths,
-        fallback: false
-    }
+      params: {
+        id: item.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps(context) {
-    const id = context?.params.id
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`) 
-    const data = await response.json()
-    
-    return {
-        props: {
-            data
-        }
-    }
+  const id = context?.params.id;
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
-
-
